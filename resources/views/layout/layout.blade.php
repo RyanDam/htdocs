ae0d21251f00683cbec5116054025b829f207a3a
RyanDam <py.ryan.dam@gmail.com>
2015-11-09 11:00:35 +0700

    update layout

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield('title')</title>
+	<link rel="stylesheet" type="text/css" href="css/toolbar.css">
+</head>
+<body>
+	@section('toolbar')
+		
+	@show
+
+	@section('content')
+
+	@show
+
+	@section('footder')
+
+	@show
+</body>
+</html>
