5cb0ea8e2add5fbf6817a41d7916ff7917fa02a2
Phi Ánh <phianhnpa1995@gmail.com>
2015-11-10 22:16:28 +0700

    update

--- resources/views/search/show.blade.php
+++ resources/views/search/show.blade.php
@@ -1,8 +1,24 @@
-<h1>Kết quả</h1>
-<ul>
-	<li>Bài một</li>
-	<p>hahahahah</p>
-	<hr>
-	<li>Bài hai</li>
-	<p>hhihihihii</p>
-</ul>
+<h1>Tim kiem</h1>
+<form action="{{ route('search') }}" method="GET">
+	<input type="text" name="searchinput" placeholder="Nhập để tìm kiếm" class="toolsearch darkgray"></input>
+	<input type="submit" value="TÌM" class="toolsearchsubmit white"></form>
+</form>
+<hr>
+
+@if (empty($result["post"]) and empty($result["bt"])) 
+	<p>Không tìm thấy kết quả</p>
+@else
+
+	<h3><a href="{{ route('bt.show', $bt->id) }}">Giống cây {{ $bt->nameNormal }}</a></h3>
+	<ul>
+		<li>{{ $bt->code }}</li>
+		<li>{{ $bt->nameNormal }}</li>
+	</ul>	
+@endforeach
+
+@foreach ($result["post"] as $post)
+	<h3><a href="{{ route('post.show', $post->id) }}"> {{ $post->title }}</a></h3>
+	<p>{{ str_limit($post->content, 250) }}</p>
+@endforeach
+
+@endif
