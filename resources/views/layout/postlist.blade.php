35bba6fa9634061ca08ecc74b4534b3549e53d1d
RyanDam <py.ryan.dam@gmail.com>
2015-11-11 10:05:28 +0700

    Theme Caytrong, Search
    - Theme Caytrong, Search result
    - Fix URL route

--- resources/views/layout/postlist.blade.php
+++ resources/views/layout/postlist.blade.php
@@ -3,7 +3,13 @@
 		<img src="img/{{$thumbnail}}" class="thumbnailitem">
 	</div>
 	<div class="contenditemholder">
-		<h2><a href="{{ route('post.show', $p->id) }}" class="fontgothambold darkgray menu">{{ $title }}</a></h2>
+
+	@if ($mode === '1')
+		<h2><a href="{{ route('bt.show', $id) }}" class="fontgothambold darkgray menu">{{$title}}</a></h2>
+	@elseif ($mode === '2')
+		<h2><a href="{{ route('post.show', $id) }}" class="fontgothambold darkgray menu">{{$title}}</a></h2>
+	@endif
+
 		<p class="postcontent fontcontent itemcontent darkgray">{{ str_limit($content, 250) }}</p>
 	</div>
 	<div class="posttypeitemholder">
